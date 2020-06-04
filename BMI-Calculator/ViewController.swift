af15946927509fbfd1e19603febac105f54b2414
Jacob Haff <jacobhaff2@gmail.com>
2020-06-04 10:49:04 -0400

    Calculate BMI and display on new SecondViewController

--- BMI-Calculator/ViewController.swift
+++ BMI-Calculator/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -24,6 +27,16 @@ class ViewController: UIViewController {
         let weight = String(format: "%.0f", sender.value)
         weightLabel.text = "\(weight)kg"    }
     
-
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let bmi = weightSlider.value / (heightSlider.value * heightSlider.value)
+        
+        let secondVC = SecondViewController()
+        
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
+    
 }
 
